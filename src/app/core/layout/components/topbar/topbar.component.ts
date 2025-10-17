5287c4560ab2d5175456138c5fba17c3f054a576
Ariel Paricagua <arielfparicaguav@gmail.com>
2025-10-17 12:52:25 -0400

    fix: Correccion de topbar en opciones

--- src/app/core/layout/components/topbar/topbar.component.ts
+++ src/app/core/layout/components/topbar/topbar.component.ts
@@ -1,4 +1,3 @@
-// topbar/topbar.component.ts
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -12,21 +11,10 @@ import { TemaService } from '../../../../services/tema.service';
 })
 export class AppTopbarComponent {
   servicioTema = inject(TemaService);
-  menuAbierto = signal(false);
   menuTemaAbierto = signal(false);
-
-  alternarMenu(): void {
-    this.menuAbierto.update(v => !v);
-    this.menuTemaAbierto.set(false);
-  }
 
   alternarMenuTema(): void {
     this.menuTemaAbierto.update(v => !v);
-  }
-
-  alternarTema(): void {
-    this.servicioTema.alternarTema();
-    this.menuTemaAbierto.set(false);
   }
 
   seleccionarTema(tema: 'claro' | 'oscuro' | 'sistema'): void {
@@ -35,7 +23,6 @@ export class AppTopbarComponent {
   }
 
   cerrarMenus(): void {
-    this.menuAbierto.set(false);
     this.menuTemaAbierto.set(false);
   }
 }
